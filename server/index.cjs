8c8903abd46df71f8529fd23062cb914e71bb6e1
Abdelilah-AIT-HAMOU <abdelilah.ait.hamou@oracle.com>
2025-02-06 22:38:48 +0100

    changed path for logo

--- server/index.cjs
+++ server/index.cjs
@@ -11,10 +11,10 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 4000;
+const port = 5501;
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0" , () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
 
 app.use(cors());
